6e78904785a039551fe19376a99662a7184af085
haideraltahan <haideraltahan@gmail.com>
2025-06-06 13:02:07 -0400

    more time

--- tests/mark.sh
+++ tests/mark.sh
@@ -4,7 +4,7 @@
 #SBATCH --mem-per-gpu=64GB
 #SBATCH --cpus-per-gpu=8
 #SBATCH --gres=gpu:8
-#SBATCH --time=12:00:00
+#SBATCH --time=48:00:00
 #SBATCH --output=./scripts_log/%A_%a.out
 #SBATCH --partition=devlab
 
