4139a5e7dece9324813e8c41283fe110ee65bd6e
marksibrahim <mark.ibhm@gmail.com>
2025-06-13 18:32:24 +0000

    updates mark test scripts

--- tests/mark/download_benchmarks.sh
+++ tests/mark/download_benchmarks.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-#SBATCH --job-name=job
+#SBATCH --job-name=unibench
 #SBATCH -N1 --ntasks-per-node=1
 #SBATCH --mem-per-gpu=64GB
-#SBATCH --cpus-per-gpu=8
-#SBATCH --gres=gpu:8
+#SBATCH --cpus-per-gpu=12
+#SBATCH --gres=gpu:1
 #SBATCH --time=48:00:00
 #SBATCH --output=./scripts_log/%A_%a.out
-#SBATCH --partition=devlab
+#SBATCH --account=a100-memorization
 
-source /private/home/marksibrahim/Projects/Unibench/unibench/unibench2/bin/activate
+source /home/marksibrahim/Projects/UniBench/unibench/.venv/bin/activate
 which python
-cd /private/home/marksibrahim/Projects/Unibench/unibench/tests/mark
+cd /home/marksibrahim/Projects/UniBench/unibench/tests/mark
 
-# export HF_HOME=/storage/home/hcoda1/6/haltahan6/p-rmurty7-0/haider/.cache/hf_home
-# export HUGGINGFACE_HUB_CACHE=/storage/home/hcoda1/6/haltahan6/p-rmurty7-0/haider/.cache/hf
-# export UNIBENCH_HUB=/storage/home/hcoda1/6/haltahan6/p-rmurty7-0/haider/.cache/unibench
-# export TORCH_HOME=/storage/home/hcoda1/6/haltahan6/p-rmurty7-0/haider/.cache/torch
+export HF_HOME=/fsx-robust/marksibrahim/datasets/hf_home
+export HUGGINGFACE_HUB_CACHE=/fsx-robust/marksibrahim/datasets/hf
+export UNIBENCH_HUB=/fsx-robust/marksibrahim/datasets/unibench
+export TORCH_HOME=/fsx-robust/marksibrahim/datasets/torch
 
 unibench download_benchmarks
